24f01911523150b2a1c60c34276f7980bf7daaf2
Sam Bentley <samtbentley@gmail.com>
2020-04-28 00:19:53 +0100

    Recoverable errors and error propagation

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,3 +1,9 @@
+use std::fs::File;
+use std::io;
+use std::fs;
+use std::io::Read;
+use std::error::Error;
+
 fn panics() {
     println!("Error Handling!");
     // panic!("Crash and burn");
@@ -5,10 +11,10 @@ fn panics() {
     v[99];
 }
 
-use std::fs::File;
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let f = File::open("hello.txt");
 
+    /*
     let f = match f {
         Ok(file) => file,
         Err(error) => match error.kind() {
@@ -20,7 +26,49 @@ fn main() {
                 panic!("Problem opening the file {:?}", error),
             },
     };
+    */
+    // return value of file or panics if it doesn't exist
+    let f = File::open("hello.txt").unwrap();
+    let f = File::open("hello.txt").expect("Failed to open file hello.txt");
+
+    let f = File::open("hello.txt")?;
+    Ok(())
+}
+
+// Propagate any error
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+// Use the shortcut ? for error propagation
+fn read_username_from_file_shorter() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+// Chain together ? expressions
+fn read_username_even_shorter() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
+
+}
 
 
-
+fn read_username_shortest() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
 }
