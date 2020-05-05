57ab9e7fdfa4a48dfa54c560defaa86d9bc8819a
Sam Bentley <samtbentley@gmail.com>
2020-05-05 23:16:07 +0100

    more grep project

--- samgrep/src/main.rs
+++ samgrep/src/main.rs
@@ -1,15 +1,18 @@
 use std::env;
-use std::fs;
+use std::process;
+use samgrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
-
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
-    println!("With text\n{}",contents);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1)
+    });
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+    if let Err(e) = samgrep::run(config) {
+        println!("Application Error {}", e);
+        process::exit(1);
+    }
 }
