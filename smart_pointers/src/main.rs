13118c880b3053598b21a1d9bdb536c7f6e47fed
Sam Bentley <samtbentley@gmail.com>
2020-05-16 17:18:48 +0100

    Smart pointers - RefCell

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,7 +1,12 @@
 use crate::List::{Cons, Nil};
 use std::ops::Deref;
+use std::rc::Rc;
+use std::cell::RefCell;
+
+mod interior_mutability;
+#[derive(Debug)]
 enum List {
-    Cons(i32, Box<List>),
+    Cons(Rc<RefCell<i32>>, Rc<List>),
     Nil,
 }
 
@@ -25,7 +30,7 @@ fn main() {
     let b = Box::new(5);
     println!("b = {}", b);
 
-    let list = Cons(1,Box::new(Cons(2,Box::new( Cons(3, Box::new(Nil))))));
+//    let list = Cons(1,Box::new(Cons(2,Box::new( Cons(3, Box::new(Nil))))));
 
     let x = 5;
     let y = &x;
@@ -45,6 +50,29 @@ fn main() {
 
     let m = MyBox::new(String::from("Rust"));
     hello(&m);
+/*
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+
+*/
+    // Combine RefCell and Rc
+    let value = Rc::new(RefCell::new(5));
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+    let b = Cons(Rc::new(RefCell::new(6)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(10)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
 }
 
 fn hello(name: &str) {
